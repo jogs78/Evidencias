e6e4d0e0d3bde9b99c1f3c91c0b6d23f487c3568
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2020-02-24 15:56:14 -0600

    24/feb/2020

---
+++ resources/views/inicio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INICIO</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <script src="/js/popper.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+    Entrar:<br>
+    <a class="btn btn-primary"  href="/estudiantes">SISTEMA PARA ESTUDIANTES</a>
+    <br>
+    <br>
+    <a class="btn btn-primary"  href="/estudiantes">SISTEMA PARA PROFESOR </a>
+</body>
+</html>
