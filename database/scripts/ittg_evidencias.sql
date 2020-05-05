8fee55266227a7966897cad5ea54b0c69d5ab891
Jorge Octavio Guzmán Sánchez <jogs78@gmail.com>
2020-05-04 22:59:32 -0500

    Cambie la longitud del email, esperando que disminuya la longitud de la key

--- database/scripts/ittg_evidencias.sql
+++ database/scripts/ittg_evidencias.sql
@@ -229,7 +229,7 @@ INSERT INTO `rubricas` (`id`, `tipo_de`, `autor`) VALUES
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
